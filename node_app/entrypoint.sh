5708488e5a16c876afdaa05676d136d7e91f697a
Thai Pham <thaiph.hust@gmail.com>
2018-02-21 16:46:01 +0700

    Update entrypoint.sh

--- node_app/entrypoint.sh
+++ node_app/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 while ! nc -w 1 fluentd 24224 > /dev/null 2>&1; do
-  echo "NodeJS Application waithing TCP connection $FLUENTD_PORT..."
+  echo "NodeJS Application waiting TCP connection $FLUENTD_PORT..."
   sleep 1
 done
 
